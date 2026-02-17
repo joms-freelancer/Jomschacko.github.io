81b633cc6c30606bd0a30b8386054433b2bd2230
joms-freelancer <123381933+joms-freelancer@users.noreply.github.com>
2026-02-17 18:54:19 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  // ഓഫ്‌ലൈൻ വർക്കിംഗിനുള്ള കോഡ് പിന്നീട് ചേർക്കാം
+});
