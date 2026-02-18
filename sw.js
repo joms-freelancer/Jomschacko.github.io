bb2b00f5ec2b1384985774214cbf6b4e3968617d
joms-freelancer <123381933+joms-freelancer@users.noreply.github.com>
2026-02-18 09:26:01 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // ഓഫ്‌ലൈൻ വർക്കിംഗിനുള്ള കോഡ് പിന്നീട് ചേർക്കാം
-});
